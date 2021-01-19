30e89f8129e07575aaca4d6ada0aac54e6f61229
Miles James <milesw.james@gmail.com>
2021-01-19 14:16:43 -0800

    Update and rename numberguessinggame.py to main.py

---
+++ main.py
@@ -0,0 +1,30 @@
+import random
+from guess import Guess
+
+def guess_the_number():
+
+    Guess.number = random.randint(1, 100)
+
+    tries = 1
+
+    while True:
+
+        Guess.guess = abs(int(input("\nGuess a number between 1 - 100: ")))   
+
+        if Guess.guess == Guess.number:
+            print("\nYou guessed the number correctly! The number was:", Guess.number)
+            print(f"It took you {tries} tries.")
+            break
+        
+        elif Guess.guess > Guess.number:
+            print("\nYour guess was too high...")
+            tries += 1
+            continue
+        
+        elif Guess.guess < Guess.number:
+            print("\nYour guess was too low...")
+            tries += 1
+            continue
+
+
+guess_the_number()
